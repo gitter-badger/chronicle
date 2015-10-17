e9a92d57bf0b335e44d58c28af156c2552493951
Elias Gabrielsson <elias@Eliass-MBP.bobbarebygg.se>
2015-10-17 19:08:11 +0200

    Added regex for .req files

--- walker/walker.go
+++ walker/walker.go
@@ -3,13 +3,29 @@ package walker
 import (
 	"fmt"
 	"log"
+	"regexp"
 
 	"github.com/libgit2/git2go"
 )
 
+var walker Walker
+
+// Walker is a container for storing results and holding working struct like regex matchers.
+type Walker struct {
+	reqMatcher *regexp.Regexp
+}
+
+// Wraper for regex matcher for .req file
+func (w *Walker) reqMatchString(s string) bool {
+	return w.reqMatcher.MatchString(s)
+}
+
 // UpdateRepo updates the local requirment database by parsing the git-historyz
 // The string points to the location of the git database and where to create chronicle database.
 func UpdateRepo(repoPath string) {
+	walker = Walker{}
+	walker.reqMatcher, _ = regexp.Compile(".*\\.req")
+
 	repo, err := git.OpenRepository(repoPath)
 	if err != nil {
 		log.Fatal(err)
@@ -18,7 +34,7 @@ func UpdateRepo(repoPath string) {
 
 	head, err := repo.Head()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	fmt.Println("Reference, head", head.Name())
 
@@ -28,14 +44,11 @@ func UpdateRepo(repoPath string) {
 	}
 	fmt.Println("Head commit", headCommit.Id())
 
-	findRoots(headCommit)
-
-	// Find root
+	rootIds := crawlRepo(headCommit)
+	fmt.Println("All roots of git tree", rootIds)
 
 	// Update all line references for old req.
 	// *Diff, err = repo.DiffTreeToTree(oldTree, newTree, options)
-
-	// Check if there is some new .req files
 
 	// Check if there is a commit req reference
 	// *Diff, err = repo.DiffIndexToWorkdir(null, options)
@@ -44,21 +57,41 @@ func UpdateRepo(repoPath string) {
 
 }
 
-func findRoots(c *git.Commit) {
-	fmt.Println("Base Case", c.ParentCount())
+func crawlRepo(c *git.Commit) []*git.Oid {
+	var rootOid []*git.Oid
 	if c.ParentCount() == 0 {
 		// base case
-		// ADD commit oid
 		fmt.Println("Root", c.Id())
-		return
+		rootOid = []*git.Oid{c.Id()}
+		return rootOid
 	}
 	for i := uint(0); i < c.ParentCount(); i++ {
 		fmt.Println("Not root", c.Id())
-		findRoots(c.Parent(i))
+		rootOid = append(rootOid, crawlRepo(c.Parent(i))...)
 	}
-	return
+	// Search for .req files
+	// -> Update database, new and removed req.
+	currentTree, err := c.Tree()
+	if err != nil {
+		log.Fatal(err)
+	}
+	currentTree.Walk(searchForReqFiles)
+
+	// Check if req->code have changed
+	// Check with parents commits tree, eg. c.Parent(i).Tree. <- OLD one c.Tree <- NEW one
+
+	// Check if there is a commit reference.
+
+	return rootOid
 }
 
-func crawlRepo(path string) {
+func searchForReqFiles(s string, entry *git.TreeEntry) int {
 
+	fmt.Println("Path to file:", s)
+	fmt.Println("Is .req file:", walker.reqMatchString(entry.Name))
+	fmt.Println("Name", entry.Name)
+	fmt.Println("Filemode", entry.Filemode)
+	fmt.Println("Type", entry.Type)
+	fmt.Println("")
+	return 0
 }
